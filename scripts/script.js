6ae49658c91873fa64d898cc30837aae1f7b1165
Inkatha <inkatha@github.com>
2016-01-16 03:00:30 -0600

    Created an easy and a hard mode

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,7 @@
-var colors = generateRandomColors(6);
+var easyMode = 3;
+var hardMode = 6;
+
+var colors = generateRandomColors(hardMode);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -6,16 +9,35 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1Display = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyButton = document.querySelector("#easyButton");
+var hardButton = document.querySelector("#hardButton");
+
 
 resetButton.addEventListener("click", function() {
 	//generate all new colors
-	colors = generateRandomColors(6);
-	//pick a new random color from array
-	pickedColor = pickColor();
-	//updates displayto reflect the new color
-	colorDisplay.textContent = pickedColor;
-	//change colors of squares on the page
-	resetColors();
+	colors = generateRandomColors(colors.length);
+	resetColors(colors.length);
+});
+
+//
+easyButton.addEventListener("click", function() {
+	easyButton.classList.add("selected");
+	hardButton.classList.remove("selected");
+	colors = generateRandomColors(easyMode);
+	resetColors(easyMode);
+	for (var i = easyMode; i < squares.length; i++) {
+		squares[i].style.display = "none";
+	}
+});
+
+hardButton.addEventListener("click", function() {
+	hardButton.classList.add("selected");
+	easyButton.classList.remove("selected");
+	colors = generateRandomColors(hardMode);
+	resetColors(hardMode);
+	for (var i = 0; i < squares.length; i++) {
+		squares[i].style.display = "";
+	}
 });
 
 colorDisplay.textContent = pickedColor;
@@ -28,7 +50,6 @@ for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("click", function() {
 		//grab color of clicked square
 		var clickedColor = this.style.background;
-
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!"
 			h1Display.style.background = clickedColor;
@@ -41,12 +62,18 @@ for (var i = 0; i < squares.length; i++) {
 	});
 }
 
-function resetColors() {
-	for (var i = 0; i < squares.length; i++) {
+function resetColors(num) {
+	//pick a new random color from array
+	pickedColor = pickColor();
+	//updates displayto reflect the new color
+	colorDisplay.textContent = pickedColor;
+	//change colors of squares on the page
+	for (var i = 0; i < num; i++) {
 		squares[i].style.background = colors[i];
 	}
 	h1Display.style.background = "#232323";
 	resetButton.textContent = "New Colors";
+	messageDisplay.textContent = "Try Again?";
 }
 
 function changeColors(color) {
