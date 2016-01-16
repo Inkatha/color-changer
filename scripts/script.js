3f1d6e40c04bf601c13d2e776d4a574d3c3c3e3f
Inkatha <inkatha@github.com>
2016-01-16 01:57:14 -0600

    Implemented random number generator and guessing functionality

--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
-
-
+var messageDisplay = document.querySelector("#message");
+var h1Display = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -23,11 +16,44 @@ for (var i = 0; i < squares.length; i++) {
 	squares[i].addEventListener("click", function() {
 		//grab color of clicked square
 		var clickedColor = this.style.background;
-		
+
 		if (clickedColor === pickedColor) {
-			alert("Correct!");
+			messageDisplay.textContent = "Correct!"
+			h1Display.style.background = clickedColor;
+			changeColors(clickedColor);
 		} else {
-			alert("Wrong!!");
+			this.style.background = "#232323";
+			messageDisplay.textContent = "Try Again";
 		}
 	});
+}
+
+function changeColors(color) {
+	for(var i = 0; i < squares.length; i++) {
+		squares[i].style.background = color;
+	}
+}
+
+function pickColor() {
+	var randomNum = Math.floor(Math.random() * colors.length);
+	return colors[randomNum];
+}
+
+function generateRandomColors(num) {
+	// make an array
+	var colors = []
+	//add num random colors to arr
+	for(var i = 0; i < num; i++) {
+		colors.push(randomColor());
+	}
+	//return that array
+	return colors;
+}
+
+function randomColor() {
+	var red = Math.floor(Math.random() * 255 + 1);
+	var green = Math.floor(Math.random() * 255 + 1);
+	var blue = Math.floor(Math.random() * 255 + 1);
+	var rgbColor = "rgb(" + red.toString() + ", " + green.toString()  + ", " + blue.toString() + ")";
+	return rgbColor
 }
